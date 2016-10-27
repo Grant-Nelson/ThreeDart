992fecd8d77de53cc1653fb368a4cf2ff568c549
dominicfrost <frostationdevice@gmail.com>
2016-10-26 18:12:29 -0600

    Added some radio button state to the url. Also cleaned up a type error ... GRANT!

--- test/common/radioGroup.dart
+++ test/common/radioGroup.dart
@@ -3,7 +3,6 @@ part of ThreeDart.test.common;
 typedef void radioSelectedHndl();
 
 class RadioGroup {
-
   String _elemId;
   html.Element _elem;
 
@@ -11,21 +10,45 @@ class RadioGroup {
     this._elem = html.document.getElementById(this._elemId);
   }
 
-  void add(String text, radioSelectedHndl hndl, [bool selected = false]) {
-    if (selected) hndl();
-    html.LabelElement label = new html.LabelElement()
-      ..style.whiteSpace = "nowrap";
+  String get selectedItem => Uri.base.queryParameters['$_elemId'];
+
+  void add(String text, radioSelectedHndl hndl, [bool selectedByDefault = false]) {
+    // When adding buttons, check the URL to see if the button added should be the selected one.
+    bool itemIsChecked = false;
+    if (selectedItem == null) {
+      if (selectedByDefault) {
+        itemIsChecked = true;
+        hndl();
+        updateUrlWithRadioButtonInfo(text);
+      }
+    } else if (selectedItem == text) {
+      itemIsChecked = true;
+      hndl();
+    }
+
+    html.LabelElement label = new html.LabelElement()..style.whiteSpace = "nowrap";
     this._elem.children.add(label);
     html.RadioButtonInputElement checkBox = new html.RadioButtonInputElement()
-      ..checked = selected
+      ..checked = itemIsChecked
       ..name = this._elemId;
     checkBox.onChange.listen((_) {
-        if (checkBox.checked) hndl();
-      });
+      if (checkBox.checked) {
+        updateUrlWithRadioButtonInfo(text);
+        hndl();
+      }
+    });
     label.children.add(checkBox);
-    html.SpanElement span = new html.SpanElement()
-      ..text = text;
+    html.SpanElement span = new html.SpanElement()..text = text;
     label.children.add(span);
     this._elem.children.add(new html.BRElement());
+  }
+
+  updateUrlWithRadioButtonInfo(text) {
+    Uri current = Uri.base;
+    Map parameters = new Map.from(current.queryParameters);
+    parameters['$_elemId'] = text;
+
+    Uri newUrl = current.replace(queryParameters: parameters);
+    html.window.history.replaceState('', '', newUrl.toString());
   }
 }
