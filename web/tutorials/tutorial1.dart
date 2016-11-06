2e58e890b932002383eefd3dcfc62bb0c76c1163
Grant Nelson <grantnelson1@gmail.com>
2016-11-06 15:45:43 -0700

    InitTut3: Finished the tutorial page 1.

--- web/tutorials/tutorial1.dart
+++ web/tutorials/tutorial1.dart
@@ -13,13 +13,8 @@ void tutorial1() {
     ..addCanvas("tutorial1")
     ..addHeader(0, "Getting Started")
     ..addPar(["The following is the [HTML|#index.html] and the [Dart|#main.dart] files ",
-      "that we will explore in this tutorial."])
-    ..addPar(["ThreeDart must have a ",
-      "[HTML canvas element|http://www.w3schools.com/graphics/canvas_intro.asp] to ",
-      "render graphics to. In this example the canvas is provided by [index.html|#index.html] ",
-      "and labelled `tutorial1`. On line 21 of [main.dart|#main.dart] the main ThreeDart class ",
-      "is created an attached to that canvas using its identifier. Below that is ",
-      "the render loop using the browser's `requestAnimationFrame`."])
+      "that we will explore in this tutorial. For this first tutorial I will just ",
+      "provide the code and then we'll go over code describing how it works."])
     ..addCode("index.html", "html", 0, [
       '<!DOCTYPE html>',
       '<html>',
@@ -29,7 +24,7 @@ void tutorial1() {
       '   <title>Tutorial 1</title>',
       '  </head>',
       '  <body>',
-      '    <canvas id="tutorial1" width="800" height="600"></canvas>'
+      '    <canvas id="tutorial1" width="800" height="600"></canvas>',
       '    <script type="application/dart" src="main.dart"></script>',
       '    <script src="packages/browser/dart.js"></script>',
       '  </body>',
@@ -48,11 +43,11 @@ void tutorial1() {
       "void main() {",
       "  ThreeDart.Entity obj = new ThreeDart.Entity()",
       "    ..shape = Shapes.cube()",
-      "    ..mover = new Movers.Rotater();",
+      "    ..mover = new Movers.Rotater()",
+      "    ..technique = new Techniques.Depth(fogStart: 3.0, fogStop: 6.0);",
       "",
       "  Scenes.EntityPass pass = new Scenes.EntityPass()",
       "    ..children.add(obj)",
-      "    ..tech = new Techniques.Depth(fogStart: 3.0, fogStop: 6.0)",
       "    ..camera.mover = new Movers.Constant(new Math.Matrix4.translate(0.0, 0.0, 5.0));",
       "",
       "  ThreeDart.ThreeDart td = new ThreeDart.ThreeDart.fromId(\"tutorial1\")",
@@ -64,15 +59,58 @@ void tutorial1() {
       "    window.requestAnimationFrame(update);",
       "  };",
       "  window.requestAnimationFrame(update);",
-      "}"]);
+      "}"])
+    ..addHeader(4, "ThreeDart")
+    ..addPar(["ThreeDart must have a ",
+      "[HTML canvas element|http://www.w3schools.com/graphics/canvas_intro.asp] to ",
+      "render graphics to. In this example the canvas is provided by [index.html|#index.html] ",
+      "and labelled `tutorial1`. On line 21 of [main.dart|#main.dart] the main ThreeDart class ",
+      "is created an attached to that canvas using its identifier. Below that is ",
+      "the render loop using the browser's `requestAnimationFrame`."])
+    ..addHeader(4, "Scene")
+    ..addPar(["To make the image, the rendering has to have something to render. ",
+      "A scene is set to ThreeDart to provide the content to render. A scene ",
+      "can be a single rendering pass or a composite of several scenes. In this ",
+      "tutorial the scene is only a single pass for rendering entities, the `EntityPass`. "])
+    ..addPar(["The EntityPass has a default render target, a `Front Target`, which renders ",
+      "directly to the canvas. On render the target will first clear its depth buffer ",
+      "and color buffer which is why the background is black."])
+    ..addPar(["The EntityPass also has a default camera, a `Perspective Camera`. ",
+      "For this example we moved the camera, using a `Constant Mover`, back so that the ",
+      "camera is off the center of the scene and looking at the center of the scene."])
+    ..addHeader(4, "Entity")
+    ..addPar(["One or more Entities can be added to the EntityPass. An `Entity` can have ",
+      "one or more other Entities added to it too. An Entity describes a shape or group ",
+      "of shapes that should be rendered in the scene."])
+    ..addPar(["In this tutorial we only add one which is what draws the cube to the scene. ",
+      "To make it draw a cube we set the shape of the Entity to a cube. The shape ",
+      "can be set to many other built-in shapes, such as sphere, torois, and knot. ",
+      "The shape can be loaded from a file or created mathmatically or procedually."])
+    ..addPar(["To make the Entity rotate a `Mover` is attached to it. For this tutorial ",
+      "we used the default `Rotater` to cause it to rotate. Movers can be grouped ",
+      "to multiply matrices togrether thus compounding the movements."])
+    ..addPar(["The `Technique` attached to the Entity defines how the shape should be ",
+      "drawn. In this tutorial we used a very simple technique, `Depth`. Depth ",
+      "simply draws the shape with two different colors based on distance from the ",
+      "camera. If the background, front target clear color, it the same as the ",
+      "far color, it can give the appearence of fog or merky water."])
+    ..addHeader(4, "Summary")
+    ..addPar(["Play around with the Rotater, Shape, and Depth to get a feel for "
+      "how the parts works. The main take away from this tutorial should be ",
+      "how to structure a basic render. A render is one or more pass which draws ",
+      "entities. The pass has a camera and target. Entities can contain several ",
+      "entities and can draw a shape. An entity can be moved with a Mover and ",
+      "rendered with a given technique."])
+    ..addPar(["In the next tutorial we will setup a material light technique ",
+      "and dive into creating more interesting scenes."]);
 
   ThreeDart.Entity obj = new ThreeDart.Entity()
     ..shape = Shapes.cube()
-    ..mover = new Movers.Rotater();
+    ..mover = new Movers.Rotater()
+    ..technique = new Techniques.Depth(fogStart: 3.0, fogStop: 6.0);
 
   Scenes.EntityPass pass = new Scenes.EntityPass()
     ..children.add(obj)
-    ..tech = new Techniques.Depth(fogStart: 3.0, fogStop: 6.0)
     ..camera.mover = new Movers.Constant(new Math.Matrix4.translate(0.0, 0.0, 5.0));
 
   ThreeDart.ThreeDart td = new ThreeDart.ThreeDart.fromId("tutorial1")
