5c30e1dfaeb8895a3c783463b2ee7c67cb025819
Grant Nelson <grantnelson1@gmail.com>
2018-10-03 00:12:29 -0600

    Updated the hypersphere example

---
+++ web/examples/hypersphere/example4D.dart
@@ -0,0 +1,137 @@
+part of hypersphere;
+
+void startup4D(String targetName) {
+  ThreeDart.ThreeDart td = new ThreeDart.ThreeDart.fromId(targetName);
+
+  Techniques.MaterialLight hypercubeTech = new Techniques.MaterialLight()
+    ..emission.color = new Math.Color3(0.4, 0.6, 0.8);
+    
+  Techniques.MaterialLight sliceTech = new Techniques.MaterialLight()
+    ..emission.color = new Math.Color3(0.8, 0.6, 0.4)
+    ..alpha.value = 0.3;
+
+  Techniques.Inspection projTech = new Techniques.Inspection()
+    ..showWireFrame = true
+    ..showFilled = true
+    ..diffuse1 = new Math.Color4(0.2, 0.3, 0.4, 0.2)
+    ..ambient1 = new Math.Color4(0.1, 0.2, 0.3, 0.2);
+
+  Movers.Constant sliceMover = new Movers.Constant();
+
+  Movers.Constant sphereScalar = new Movers.Constant();
+
+  Movers.Group sliceRotation = new Movers.Group()
+    ..add(new Movers.Constant.rotateX(0.4))
+    ..add(new Movers.Constant.rotateY(0.4));
+    
+  Movers.Group projUserMover = new Movers.Group();
+
+  Shapes.Shape shape = new Shapes.Shape();
+  Shapes.Vertex v1 = shape.vertices.addNewLoc( 1.0, -1.0,  1.0);
+  Shapes.Vertex v2 = shape.vertices.addNewLoc( 1.0,  1.0,  1.0);
+  Shapes.Vertex v3 = shape.vertices.addNewLoc( 1.0,  1.0, -1.0);
+  Shapes.Vertex v4 = shape.vertices.addNewLoc( 1.0, -1.0, -1.0);
+
+  Shapes.Vertex v5 = shape.vertices.addNewLoc(-1.0, -1.0,  1.0);
+  Shapes.Vertex v6 = shape.vertices.addNewLoc(-1.0,  1.0,  1.0);
+  Shapes.Vertex v7 = shape.vertices.addNewLoc(-1.0,  1.0, -1.0);
+  Shapes.Vertex v8 = shape.vertices.addNewLoc(-1.0, -1.0, -1.0);
+
+  Shapes.Vertex v9 = shape.vertices.addNewLoc( 0.5, -0.5,  0.5);
+  Shapes.Vertex v10 = shape.vertices.addNewLoc( 0.5,  0.5,  0.5);
+  Shapes.Vertex v11 = shape.vertices.addNewLoc( 0.5,  0.5, -0.5);
+  Shapes.Vertex v12 = shape.vertices.addNewLoc( 0.5, -0.5, -0.5);
+
+  Shapes.Vertex v13 = shape.vertices.addNewLoc(-0.5, -0.5,  0.5);
+  Shapes.Vertex v14 = shape.vertices.addNewLoc(-0.5,  0.5,  0.5);
+  Shapes.Vertex v15 = shape.vertices.addNewLoc(-0.5,  0.5, -0.5);
+  Shapes.Vertex v16 = shape.vertices.addNewLoc(-0.5, -0.5, -0.5);
+
+  shape.lines.addLines([
+    v1,  v2,  v2,  v3,  v3,  v4,  v4,  v1,
+    v5,  v6,  v6,  v7,  v7,  v8,  v8,  v5,
+    v9,  v10, v10, v11, v11, v12, v12, v9,
+    v13, v14, v14, v15, v15, v16, v16, v13,
+    v1,  v5,  v2,  v6,  v3,  v7,  v4,  v8,
+    v9,  v13, v10, v14, v11, v15, v12, v16,
+    v1,  v9,  v2,  v10, v3,  v11, v4,  v12,
+    v5,  v13, v6,  v14, v7,  v15, v8,  v16]);
+
+  ThreeDart.Entity hypercube = new ThreeDart.Entity()
+    ..technique = hypercubeTech
+    ..shape = shape
+    ..mover = sliceRotation;
+    
+  ThreeDart.Entity slice = new ThreeDart.Entity()
+    ..technique = sliceTech
+    ..shape = Shapes.square()
+    ..mover = (new Movers.Group()
+      ..add(new Movers.Constant.scale(1.1, 1.1, 1.1))
+      ..add(new Movers.Constant.rotateX(-Math.PI_2))
+      ..add(sliceMover)
+      ..add(sliceRotation));
+
+  ThreeDart.Entity cube = new ThreeDart.Entity()
+    ..shape = Shapes.cube()
+    ..mover = projUserMover;
+
+  ThreeDart.Entity sphere = new ThreeDart.Entity()
+    ..shape = Shapes.isosphere(2)
+    ..mover = (new Movers.Group()
+      ..add(sphereScalar)
+      ..add(projUserMover));
+
+  // Create left pass
+  Scenes.EntityPass slicePass = new Scenes.EntityPass()
+    ..target = new Views.FrontTarget(clearColor: false)
+    ..camera = (new Views.Perspective()
+      ..premover = new Movers.Constant.translate(-0.5, 0.0, 0.0)
+      ..mover = new Movers.Constant.translate(0.0, 0.0, 5.0))
+    ..children.add(hypercube)
+    ..children.add(slice);
+
+  // Create right pass
+  Scenes.EntityPass projPass = new Scenes.EntityPass()
+    ..camera =  (new Views.Perspective()
+      ..premover = new Movers.Constant.translate(0.5, 0.0, 0.0)
+      ..mover = new Movers.Constant.translate(0.0, 0.0, 5.0))
+    ..technique = projTech
+    ..children.add(sphere)
+    ..children.add(cube);
+
+  // Add the left side slider control.
+  double wOffset =0.0;
+  td.userInput.mouse.move.add((Events.EventArgs e) {
+    Input.MouseEventArgs ms = e as Input.MouseEventArgs;
+    if (!ms.button.has(Input.Button.left)) return;
+    // If on the right size, don't move the slider.
+    if (ms.adjustedPoint.x > 0.0) return;
+
+    wOffset += ms.adjustedDelta.dy;
+    wOffset = Math.clampVal(wOffset, -0.1, 1.1);
+
+    sliceMover.matrix = new Math.Matrix4.translate(0.0, 1.0-2.0*wOffset, 0.0);
+
+    if ((wOffset <= 0.0) || (wOffset >= 1.0)) {
+      cube.enabled = false;
+      sphere.enabled = false;
+    } else {
+      double r = sin(wOffset*Math.PI);
+      sphereScalar.matrix = new Math.Matrix4.scale(r, r, r);
+      cube.enabled = true;
+      sphere.enabled = true;
+    }
+
+    // On the left side so don't let mouse move continue to rotater.
+    ms.propagate = false;
+  });
+
+  // Add the right side user rotater after the left side.
+  projUserMover
+    ..add(new Movers.UserRotater(input: td.userInput, invertY: true))
+    ..add(new Movers.UserRoller(input: td.userInput, ctrl: true))
+    ..add(new Movers.UserZoom(input: td.userInput));
+    
+  // Add the two parts of the scene to the ouput.
+  td.scene = new Scenes.Compound(passes: [projPass, slicePass]);
+}
--- web/examples/hypersphere/main.dart
+++ web/examples/hypersphere/main.dart
@@ -1,7 +1,6 @@
 library hypersphere;
 
 import 'dart:math';
-import 'dart:async';
 
 import 'package:ThreeDart/ThreeDart.dart' as ThreeDart;
 import 'package:ThreeDart/Shapes.dart' as Shapes;
@@ -10,10 +9,13 @@ import 'package:ThreeDart/Math.dart' as Math;
 import 'package:ThreeDart/Techniques.dart' as Techniques;
 import 'package:ThreeDart/Scenes.dart' as Scenes;
 import 'package:ThreeDart/Views.dart' as Views;
+import 'package:ThreeDart/Events.dart' as Events;
+import 'package:ThreeDart/Input.dart' as Input;
 import '../../common/common.dart' as common;
 
-part 'projection.dart';
-part 'slice.dart';
+part 'example2D.dart';
+part 'example3D.dart';
+part 'example4D.dart';
 
 void main() {
   new common.ShellPage("Hypersphere")
@@ -22,10 +24,14 @@ void main() {
       "so little space of the hypercube."])
     ..addHeader(3, "2D: circle and square")
     ..addPar([
-      "Imagine taking slices out of a circle inscribed in a cube. Each slice is ",
+      "Imagine taking slices out of a circle inscribed in a square. Each slice is ",
       "two lines, one from the circle and one from the square. When the slices ",
-      "are near the top the line caused by the circle is small compared to the square. ",
-      "The corners of the square aren't covered."])
+      "are near the top, the line caused by the circle is small compared to the square. ",
+      "The corners of the square aren't covered by the circle."])
+    ..addLargeCanvas("target2D")
+    ..addPar([
+      "_Click and drag to move the location of the slice._"])
+
     ..addHeader(3, "3D: sphere and cube")
     ..addPar([
       "Now imagine taking slices out of a sphere inscribed in a cube. Each slice is ",
@@ -33,41 +39,29 @@ void main() {
       "the sides of the square. When the slice is near a side, the circle is almost a ",
       "point but the square is still the same size. That is a lot of extra space for ",
       "for the square not used by the circle."])
+    ..addLargeCanvas("target3D")
+    ..addPar([
+      "_Click and drag to move the location of the slice._"])
+
     ..addHeader(3, "4D: hypersphere and hypercube")
     ..addPar([
       "Now imagine taking a slice out of a hypersphere inscribed in a hypercube. ",
       "Each slice is a sphere and a cube (as shown in the graphics below). ",
-      "When the slice is near the edges the sphere is a small point in the middle ",
-      "of a cube. As the slice moves down the sphere gets bigger until it touches ",
+      "When the slice is near the edges, the sphere is a small point in the middle ",
+      "of a cube. As the slice moves down the sphere gets bigger until it touches all ",
       "the sides of the cube, then it shrinks again. Once again, that's a lot of ",
       "space in the cube not filled by the sphere."])
-    ..addLargeCanvas("targetCanvas")
+    ..addLargeCanvas("target4D")
     ..addPar([
-      "_The hypercube on the left is an (poorly done) artistic representation._ ",
-      "_Click and drag on graphics to rotate the resulting 3D slice._"])
+      "_The shape on the left is an artistic representation of a 4D hypercube._ ",
+      "_Click and drag on the left to move the location of the slice._",
+      "_Click and drag on the right to rotate the resulting 3D slice._"])
     ..addPar([
       "With each new dimension the hypersphere is small near the edges and only touching ",
       "the sides of the hypersphere in the very middle."])
     ..addPar(["«[Back to Examples|../]"]);
 
-  // Note: This was a quick thrown together example which is still in progress.
-  //       This example should have graphics for 2D and 3D too.
-  //       It needs some cleanup and commenting.
-  //       The slice could be moved manually like a slider.
-
-  ThreeDart.ThreeDart td = new ThreeDart.ThreeDart.fromId("targetCanvas");
-
-  Projection proj = new Projection(td);
-  Slice slice = new Slice(td);
-  td.scene = new Scenes.Compound(passes: [proj.pass, slice.pass]);
-
-  double wOffset = 0.5;
-  new Timer.periodic(const Duration(milliseconds: 50), (_) {
-    wOffset += 0.01;
-    if (wOffset >= 1.01) wOffset = -0.01;
-    proj.setWOffset(wOffset);
-    slice.setWOffset(wOffset);
-  });
-
-  common.showFPS(td);
+  startup2D("target2D");
+  startup3D("target3D");
+  startup4D("target4D");
 }
