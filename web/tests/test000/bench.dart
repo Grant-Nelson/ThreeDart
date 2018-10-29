37a08888e760dbaf9bf43092f80a7370ed0dfda6
Grant Nelson <grantnelson1@gmail.com>
2018-10-28 20:00:17 -0600

    Added test

--- web/tests/test000/bench.dart
+++ web/tests/test000/bench.dart
@@ -4,7 +4,7 @@ void addBench(TestManager tests) {
 
   tests.add("Uint8List bench timing", (TestArgs args) {
     data.Uint8List temp = new data.Uint8List(1000);
-    for (int i = 0; i < 400; i++) {
+    for (int i = 0; i < 100; i++) {
       for (int k = 0; k < 1000; k++) temp[k]=0;
 
       for (int j = 0; j < 100; j++) {
@@ -16,7 +16,7 @@ void addBench(TestManager tests) {
   
   tests.add("List int bench timing", (TestArgs args) {
     List<int> temp = new List<int>.filled(1000, 0);
-    for (int i = 0; i < 400; i++) {
+    for (int i = 0; i < 100; i++) {
       for (int k = 0; k < 1000; k++) temp[k]=0;
 
       for (int j = 0; j < 100; j++) {
