9d931b8746c23093163a8d408aba519517ad2cfb
Grant Nelson <grantnelson1@gmail.com>
2020-07-05 21:28:46 -0600

    Working on Octree

--- lib/src/Shapes/Octree.dart
+++ lib/src/Shapes/Octree.dart
@@ -5,24 +5,82 @@ part of ThreeDart.Shapes;
 class Octree {
   //static const int _maxDepth = 52;
 
+  Shape _shape;
   Node _root;
   Path _rootPath;
+  int _rootPathDepth;
+  int _vertexCount;
 
   /// The maximum cube that the shape can occupy.
   final Math.Cube maxCube;
   
   /// Creates a new octree.
-  Octree._(this.maxCube) {
+  Octree._(Shape this._shape, Math.Cube this.maxCube) {
     this._root = null;
     this._rootPath = null;
+    this._rootPathDepth = 0;
+    this._vertexCount = 0;
   }
 
   /// Creates a new octree with an optional maximum cube for a shape.
-  factory Octree({Math.Cube maxCube = null}) =>
-    new Octree._(new Math.Cube(-5000.0, -5000.0, -5000.0, 10000.0));
+  factory Octree(Shape shape, {Math.Cube maxCube = null}) =>
+    new Octree._(shape, maxCube ?? new Math.Cube(-5000.0, -5000.0, -5000.0, 10000.0));
 
-  bool insertVertex(Vertex v) {
-    
+  /// Adds a new [vertex] to this collection.
+  /// True if it was added, false otherwise.
+  bool add(Vertex vertex) {
+        if (vertex.shape != null) {
+      if (vertex.shape == this._shape) return false;
+      throw new Exception("May not add a vertex already attached to another shape to this shape.");
+    }
+    vertex._index = this._vertexCount;
+    vertex._shape = this._shape;
 
+    Path path = new Path.fromPoint(vertex.location, this.maxCube);
+    this._root = this._addVertex(this._root, path, vertex);
+
+    this._vertexCount++;
+    this._shape.onVertexAdded(vertex);
+    return true;
+  }
+
+  /// Creates and adds a new vertex to this collection with the default values.
+  /// If vertex already existed then null is returned, otherwise the new vertex is returned.
+  Vertex addNew({Data.VertexType type: null,
+                 Math.Point3 loc: null, Math.Vector3 norm: null, Math.Vector3 binm: null,
+                 Math.Point2 txt2D: null, Math.Vector3 txtCube: null, Math.Color4 clr: null,
+                 double weight: 0.0, Math.Point4 bending: null}) {
+    Vertex ver = new Vertex(type: type, loc: loc, norm: norm, binm: binm, txt2D: txt2D,
+      txtCube: txtCube, clr: clr, weight: weight, bending: bending);
+    if (this.add(ver)) return ver;
+    return null;
+  }
+
+  /// Creates and adds a new vertex with the given location values.
+  /// If vertex already existed then null is returned, otherwise the new vertex is returned.
+  Vertex addNewLoc(double locX, double locY, double locZ) {
+    Vertex ver = new Vertex(loc: new Math.Point3(locX, locY, locZ));
+    if (this.add(ver)) return ver;
+    return null;
+  }
+  
+  /// Adds a list of vertices to the collection.
+  void addList(List<Vertex> verList) {
+    for (Vertex ver in verList) this.add(ver);
+  }
+  
+  /// Determines if the collection has any vertices in it.
+  bool get isEmpty => this._vertexCount <= 0;
+
+  /// Determines the number of vertices in the collection.
+  int get length => this._vertexCount;
+
+
+  Node _addVertex(Node node, Path path, Vertex vertex) {
+
+
+
+
+    return node;
   }
 }
--- lib/src/Shapes/Path.dart
+++ lib/src/Shapes/Path.dart
@@ -2,36 +2,101 @@ part of ThreeDart.Shapes;
 
 /// The path to a node in the octree.
 class Path {
-  final int _x;
-  final int _y;
-  final int _z;
+  /// The maximum allowed depth value.
+  static const int maxDepth = 31;
 
-  factory Path.fromPoint(Math.Point3 loc, Math.Cube maxCube) =>
-    new Path._(
-      ((loc.x - maxCube.x)/maxCube.size).round(),
-      ((loc.y - maxCube.y)/maxCube.size).round(),
-      ((loc.z - maxCube.z)/maxCube.size).round());
+  /// The maximum allowed value for any component, 2^32 - 1.
+  static const int maxValue = 4294967295;
 
-  factory Path(int x, int y, int z, int depth) {
-    if (x < 0 || y < 0 || z < 0)
-      throw new Exception("A path must have positive coordinate ($x, $y, $z)");
-    return new Path._(x, y, z);
+  /// The x component of the path.
+  final int x;
+  
+  /// The y component of the path.
+  final int y;
+
+  /// The z component of the path.
+  final int z;
+  
+  /// The depth of the path to read the components.
+  final int depth;
+
+  /// Determines the path to the given location within the given maximum cube.
+  factory Path.fromPoint(Math.Point3 loc, Math.Cube maxCube, [int depth = maxDepth]) {
+    double scalar = maxDepth / maxCube.size;
+    return new Path(
+      ((loc.x - maxCube.x)*scalar).round(),
+      ((loc.y - maxCube.y)*scalar).round(),
+      ((loc.z - maxCube.z)*scalar).round(),
+      depth);
   }
 
-  Path._(this._x, this._y, this._z);
+  /// Constructs a path with the given coordinates and depth.
+  factory Path(int x, int y, int z, [int depth = maxDepth]) {
+    if (x < 0 || x > maxValue)
+      throw new Exception("X component in the path must be between 0 and $maxValue ($x)");
+    if (y < 0 || y > maxValue)
+      throw new Exception("Y component in the path must be between 0 and $maxValue ($y)");
+    if (z < 0 || z > maxValue)
+      throw new Exception("Z component in the path must be between 0 and $maxValue ($z)");
+    if (depth < 0 || depth > maxDepth)
+      throw new Exception("Depth in the path must be between 0 and $maxDepth ($depth)");
+    return new Path._(x, y, z, depth);
+  }
 
-  Math.Point3 location(Math.Cube maxCube) =>
-    new Math.Point3(
-      this._x * maxCube.size + maxCube.x,
-      this._y * maxCube.size + maxCube.y,
-      this._z * maxCube.size + maxCube.z);
+  /// Internal constructor to assign final values.
+  Path._(this.x, this.y, this.z, this.depth);
 
-  int childIndexAt(int index) {
-    int mask = 1 << index;
+  /// Determines the common path between this and the other path.
+  Path common(Path other) {
+    int depth = (this.depth < other.depth)? this.depth: other.depth;
+    int diff = (this.x ^ other.x) | (this.y ^ other.y) | (this.z ^ other.z);
+    int fullMask = 0x00;
+    for (int d = 0, mask = 0x01; d < depth; d++, mask <<= 1) {
+      if (diff & mask != 0x00) {
+        depth = d;
+        break;
+      }
+      fullMask |= mask;
+    }
+    return new Path(this.x & fullMask, this.y & fullMask, this.z & fullMask, depth);
+  }
+
+  /// This gets the location this path is for in the given maximum cube.
+  /// This does not take into account the depth.
+  Math.Point3 location(Math.Cube maxCube) {
+    double scalar = maxCube.size / maxDepth;
+    return new Math.Point3(
+      this.x * scalar + maxCube.x,
+      this.y * scalar + maxCube.y,
+      this.z * scalar + maxCube.z);
+  }
+
+  /// Gets the octree child index, 0 to 7, to take in this path at the given depth.
+  int childIndexAt(int depth) {
+    int mask = 1 << depth;
     int childIndex = 0;
-    if (this._x & mask != 0x00) childIndex |= 1;
-    if (this._y & mask != 0x00) childIndex |= 2;
-    if (this._z & mask != 0x00) childIndex |= 4;
+    if (this.x & mask != 0x00) childIndex |= 1;
+    if (this.y & mask != 0x00) childIndex |= 2;
+    if (this.z & mask != 0x00) childIndex |= 4;
     return childIndex;
+  }
+
+  /// Gets the string for this path in binary.
+  String toString() {
+    StringBuffer xstr = new StringBuffer();
+    StringBuffer ystr = new StringBuffer();
+    StringBuffer zstr = new StringBuffer();
+    for (int d = 0, x = 0, mask = 0x01; d < this.depth; d++, x++, mask <<= 1) {
+      if (x == 4) {
+        x = 0;
+        xstr.write(" ");
+        ystr.write(" ");
+        zstr.write(" ");
+      }
+      xstr.write((this.x & mask != 0x00)? "1": "0");
+      ystr.write((this.y & mask != 0x00)? "1": "0");
+      zstr.write((this.z & mask != 0x00)? "1": "0");
+    }
+    return "[${xstr.toString()}, ${ystr.toString()}, ${zstr.toString()}]";
   }
 }
