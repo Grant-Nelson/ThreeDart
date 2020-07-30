dc3f91555ef74c5e445fa845d4e2c1d7a1c08e8d
Grant Nelson <grantnelson1@gmail.com>
2020-07-30 00:49:42 -0600

    Added benchmarks to tests

--- web/tests/test000/testTools.dart
+++ web/tests/test000/testTools.dart
@@ -4,6 +4,9 @@ part of ThreeDart.test.test000;
 /// [args] are provided to call-back the status of the test.
 typedef void TestHandler(TestArgs args);
 
+/// The method handler for unit-tests when benchmarking.
+typedef void TestBenchHandler();
+
 /// The interface for the unit-test to callback with.
 abstract class TestArgs {
 
@@ -29,6 +32,10 @@ abstract class TestArgs {
   /// Prints text to the test's output console as an error.
   /// This will also mark this test as a failure.
   void error(String text);
+  
+  /// Runs a benchmark for the approximately amount of time
+  /// then prints the results of the benchmark.
+  void bench(double seconds, TestBenchHandler hndl);
 }
 
 /// The block for the unit-test output and the test arguments.
@@ -47,20 +54,20 @@ class TestBlock extends TestArgs {
 
   /// Creates a new test block for the given test.
   TestBlock(this._man, this._skip, this._test, this._testName) {
-      this._body = new html.DivElement()
-        ..className = "test_body body_hidden";
-      this._title = new html.DivElement()
-        ..className = "running top_header"
-        ..onClick.listen(this._titleClicked);
-      this._man._elem.children
-        ..add(this._title)
-        ..add(this._body);
-      this._start = null;
-      this._end = null;
-      this._started = false;
-      this._failed = false;
-      this._finished = false;
-      this._update();
+    this._body = new html.DivElement()
+      ..className = "test_body body_hidden";
+    this._title = new html.DivElement()
+      ..className = "running top_header"
+      ..onClick.listen(this._titleClicked);
+    this._man._elem.children
+      ..add(this._title)
+      ..add(this._body);
+    this._start = null;
+    this._end = null;
+    this._started = false;
+    this._failed = false;
+    this._finished = false;
+    this._update();
   }
 
   /// Handles the test title clicked to show and hide the test output.
@@ -108,7 +115,7 @@ class TestBlock extends TestArgs {
     new asy.Future(() {
       this._started = true;
       this._update();
-    }).then((_){
+    }).then((_) {
       this._start = new DateTime.now();
       if (!this._skip) this._test(this);
       this._end = new DateTime.now();
@@ -116,7 +123,7 @@ class TestBlock extends TestArgs {
       this._end = new DateTime.now();
       this.error("\nException: $exception");
       this.warning("\nStack: $stackTrace");
-    }).then((_){
+    }).then((_) {
       this._finished = true;
       this._man._testDone(this);
       this._update();
@@ -180,110 +187,129 @@ class TestBlock extends TestArgs {
       this._update();
     }
   }
+
+  /// Runs a benchmark for the approximately amount of time
+  /// then prints the results of the benchmark.
+  void bench(double seconds, TestBenchHandler hndl) {
+    DateTime start = new DateTime.now();
+    double duration = 0.0;
+    int count = 0;
+    while (duration < seconds) {
+      hndl();
+      count++;
+      Duration diff = new DateTime.now().difference(start);
+      duration = diff.inMilliseconds / Duration.millisecondsPerSecond;
+    }
+    notice("Benchmark results:\n");
+    notice("  count    = $count\n");
+    notice("  duration = $duration secs\n");
+    notice("  average  = ${duration/count} secs\n");
+    notice("  estimate = ${count/duration} per sec\n");
+  }
 }
 
 /// The manager to run the tests.
 class TestManager {
-    html.Element _elem;
-    html.DivElement _header;
-    convert.HtmlEscape _escape;
-    DateTime _start;
-    List<TestBlock> _tests;
-    int _finished;
-    int _failed;
-    String _prefix;
+  html.Element _elem;
+  html.DivElement _header;
+  convert.HtmlEscape _escape;
+  DateTime _start;
+  List<TestBlock> _tests;
+  int _finished;
+  int _failed;
+  String _prefix;
 
-    /// Creates new test manager attached to the given element.
-    TestManager(this._elem) {
-      this._escape = new convert.HtmlEscape(convert.HtmlEscapeMode.element);
-      this._header = new html.DivElement();
-      this._elem.children.add(this._header);
-      html.DivElement checkBoxes = new html.DivElement()
-        ..className = "log_checkboxes";
-      this._createLogSwitch(checkBoxes, "Information", "info_log");
-      this._createLogSwitch(checkBoxes, "Notice", "notice_log");
-      this._createLogSwitch(checkBoxes, "Warning", "warning_log");
-      this._createLogSwitch(checkBoxes, "Error", "error_log");
-      this._elem.children.add(checkBoxes);
-      this._start = new DateTime.now();
-      this._tests = new List<TestBlock>();
-      this._finished = 0;
-      this._failed = 0;
-      this._prefix = "";
-    }
+  /// Creates new test manager attached to the given element.
+  TestManager(this._elem) {
+    this._escape = new convert.HtmlEscape(convert.HtmlEscapeMode.element);
+    this._header = new html.DivElement();
+    this._elem.children.add(this._header);
+    html.DivElement checkBoxes = new html.DivElement()
+      ..className = "log_checkboxes";
+    this._createLogSwitch(checkBoxes, "Information", "info_log");
+    this._createLogSwitch(checkBoxes, "Notice", "notice_log");
+    this._createLogSwitch(checkBoxes, "Warning", "warning_log");
+    this._createLogSwitch(checkBoxes, "Error", "error_log");
+    this._elem.children.add(checkBoxes);
+    this._start = new DateTime.now();
+    this._tests = new List<TestBlock>();
+    this._finished = 0;
+    this._failed = 0;
+    this._prefix = "";
+  }
 
-    /// The filter to only let tests with the given prefix to be run.
-    /// Set to empty to run all tests.
-    String get testPrefixFilter => this._prefix;
-    set testPrefixFilter(String prefix) => this._prefix = prefix;
+  /// The filter to only let tests with the given prefix to be run.
+  /// Set to empty to run all tests.
+  String get testPrefixFilter => this._prefix;
+  set testPrefixFilter(String prefix) => this._prefix = prefix;
 
-    /// Creates a check box for changing the visibility of logs with the given [type].
-    void _createLogSwitch(html.DivElement checkBoxes, String text, String type) {
-      html.CheckboxInputElement checkBox = new html.CheckboxInputElement()
-        ..className = "log_checkbox"
-        ..checked = true;
-      checkBox.onChange.listen((_) {
-          html.ElementList<html.Element> myElements = html.document.querySelectorAll(".$type");
-          String display = checkBox.checked? "block": "none";
-          for (int i = 0; i < myElements.length; i++) {
-              myElements[i].style.display = display;
-          }
-        });
-      checkBoxes.children.add(checkBox);
-      html.SpanElement span = new html.SpanElement()
-        ..text = text;
-      checkBoxes.children.add(span);
-    }
-
-    /// Callback from a test to indicate it is done
-    /// and to have the manager start a new test.
-    void _testDone(TestBlock block) {
-      this._finished++;
-      if (block.failed) this._failed++;
-      this._update();
-      if (this._finished < this._tests.length) {
-        asy.Timer.run(this._tests[this._finished].run);
-      }
-    }
-
-    /// Updates the top header of the tests.
-    void _update() {
-      String time = ((new DateTime.now().difference(this._start).inMilliseconds)*0.001).toStringAsFixed(2);
-      int testCount = this._tests.length;
-      if (testCount <= this._finished) {
-        if (this._failed > 0) {
-          this._header.className = "top_header failed";
-          if (this._failed == 1)
-            this._header.text = "Failed 1 Test (${time}s)";
-          else this._header.text = "Failed ${this._failed} Tests (${time}s)";
-        } else {
-          this._header
-            ..text = "Tests Passed (${time}s)"
-            ..className = "top_header passed";
+  /// Creates a check box for changing the visibility of logs with the given [type].
+  void _createLogSwitch(html.DivElement checkBoxes, String text, String type) {
+    html.CheckboxInputElement checkBox = new html.CheckboxInputElement()
+      ..className = "log_checkbox"
+      ..checked = true;
+    checkBox.onChange.listen((_) {
+        html.ElementList<html.Element> myElements = html.document.querySelectorAll(".$type");
+        String display = checkBox.checked? "block": "none";
+        for (int i = 0; i < myElements.length; i++) {
+            myElements[i].style.display = display;
         }
+      });
+    checkBoxes.children.add(checkBox);
+    html.SpanElement span = new html.SpanElement()
+      ..text = text;
+    checkBoxes.children.add(span);
+  }
+
+  /// Callback from a test to indicate it is done
+  /// and to have the manager start a new test.
+  void _testDone(TestBlock block) {
+    this._finished++;
+    if (block.failed) this._failed++;
+    this._update();
+    if (this._finished < this._tests.length) {
+      asy.Timer.run(this._tests[this._finished].run);
+    }
+  }
+
+  /// Updates the top header of the tests.
+  void _update() {
+    String time = ((new DateTime.now().difference(this._start).inMilliseconds)*0.001).toStringAsFixed(2);
+    int testCount = this._tests.length;
+    if (testCount <= this._finished) {
+      if (this._failed > 0) {
+        this._header.className = "top_header failed";
+        if (this._failed == 1)
+          this._header.text = "Failed 1 Test (${time}s)";
+        else this._header.text = "Failed ${this._failed} Tests (${time}s)";
       } else {
-        String prec = ((this._finished.toDouble()/testCount)*100.0).toStringAsFixed(2);
-        this._header.text = "Running Tests: ${this._finished}/${testCount} ($prec%)";
-        if (this._failed > 0) {
-          this._header
-            ..text = "${this._header.text} - ${this._failed} failed)"
-            ..className = "topHeader failed";
-        } else {
-          this._header.className = "topHeader running";
-        }
+        this._header
+          ..text = "Tests Passed (${time}s)"
+          ..className = "top_header passed";
+      }
+    } else {
+      String prec = ((this._finished.toDouble()/testCount)*100.0).toStringAsFixed(2);
+      this._header.text = "Running Tests: ${this._finished}/${testCount} ($prec%)";
+      if (this._failed > 0) {
+        this._header
+          ..text = "${this._header.text} - ${this._failed} failed)"
+          ..className = "topHeader failed";
+      } else {
+        this._header.className = "topHeader running";
       }
     }
+  }
 
-    /// Adds a new test to be run.
-    void add(String testName, TestHandler test, {bool skip = false}) {
-      if (testName.length <= 0) testName = "$test";
-      if (!testName.startsWith(this._prefix)) return;
-      this._tests.add(new TestBlock(this, skip, test, testName));
-      this._update();
+  /// Adds a new test to be run.
+  void add(String testName, TestHandler test, {bool skip = false}) {
+    if (testName.length <= 0) testName = "$test";
+    if (!testName.startsWith(this._prefix)) return;
+    this._tests.add(new TestBlock(this, skip, test, testName));
+    this._update();
 
-      // If currently none are running, start this one.
-      if (this._finished + 1 == this._tests.length) {
-        asy.Timer.run(this._tests[this._finished].run);
-      }
+    // If currently none are running, start this one.
+    if (this._finished + 1 == this._tests.length) {
+      asy.Timer.run(this._tests[this._finished].run);
     }
+  }
 }
